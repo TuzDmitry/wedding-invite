b0b684982a2468cc4511e7004677d4b7c66f7cd1
Tuz <dmitry.tuz@awtg.co.uk>
2025-05-23 00:46:06 +0300

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    trailingSlash: true,
+    basePath: "/wedding-invite",
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
