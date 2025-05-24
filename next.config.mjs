e0ebb1304151497b725a2105bfc5094fe40e030c
Tuz <dmitry.tuz@awtg.co.uk>
2025-05-24 15:12:15 +0300

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
     trailingSlash: true,
     basePath: "/wedding-invite",
     assetPrefix: '/wedding-invite',
-    images: {
-        unoptimized: true,
-    },
+    // images: {
+    //     unoptimized: true,
+    // },
 };
 
 export default nextConfig;
