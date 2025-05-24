bbcd16826f8849df32774b565ae98a121cbb57b9
Tuz <dmitry.tuz@awtg.co.uk>
2025-05-24 15:44:32 +0300

    check with fnc

--- src/app/[inviteId]/page.tsx
+++ src/app/[inviteId]/page.tsx
@@ -10,9 +10,9 @@ export default function InvitePage({ params }: { params: { inviteId: string } })
     const { inviteId } = params;
 
     // Если параметр невалидный — вызываем 404
-    // if (!guestIds.includes(inviteId)) {
-    //     notFound(); // ⛔️ важная строка
-    // }
+    if (!guestIds.includes(inviteId)) {
+        notFound(); // ⛔️ важная строка
+    }
 
     const title = GuestsTitleMap[inviteId as keyof typeof GuestsTitleMap];
     return <PageContent guestTitle={title} />;
