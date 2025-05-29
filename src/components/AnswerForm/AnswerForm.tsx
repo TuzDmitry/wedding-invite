67ed13b6483aaa49a4db6c6d9bf7c995296d506f
Tuz <dmitry.tuz@awtg.co.uk>
2025-05-29 03:30:14 +0300

    fix recapcha

--- src/components/AnswerForm/AnswerForm.tsx
+++ src/components/AnswerForm/AnswerForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { MutableRefObject, useRef } from "react";
+import React, { useRef } from "react";
 import { useFormState } from "react-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -9,34 +9,26 @@ import { saveGuest } from "@/app/actions/saveGuest";
 export default function AnswerForm({invite_id}: { invite_id: string }) {
     const [state, action] = useFormState(
         saveGuest.bind(null, invite_id),
-        {success: false},
+        {success: false}
     );
 
-    const recaptchaRef: MutableRefObject<ReCAPTCHA | null> = useRef(null)
+    const formRef = useRef<HTMLFormElement>(null);
+    const recaptchaRef = useRef<ReCAPTCHA | null>(null);
     const hiddenTokenRef = useRef<HTMLInputElement>(null);
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        console.log('event')
-        e.preventDefault();
-
-        const form = e.currentTarget;
-
+    const handleSubmit = async () => {
         const token = await recaptchaRef.current?.executeAsync();
         if (!token) return;
-
         const tokenInput = hiddenTokenRef.current;
 
         if (tokenInput) tokenInput.value = token;
 
-        recaptchaRef.current?.reset();
-
-        // form.requestSubmit();
+        formRef.current?.requestSubmit();
     };
-
 
     return (
         <form
-            onSubmit={handleSubmit}
+            ref={formRef}
             action={action}
             className="flex flex-col w-full"
         >
@@ -47,23 +39,30 @@ export default function AnswerForm({invite_id}: { invite_id: string }) {
             />
             <h3 className="text-lg">Уточните ваше присутвие</h3>
             <input
-                className='bg-transparent outline-none border-[1.5px] border-black p-3 w-full mb-4'
+                className="bg-transparent outline-none border-[1.5px] border-black p-3 w-full mb-4"
                 disabled={state.success}
-                // errorMessage={formStateErrors.title?.join(", ")}
-                // isInvalid={!!formStateErrors.title}
                 name="rsvp_text"
                 maxLength={100}
                 required
             />
-            <input type="hidden" name="g-recaptcha-response" ref={hiddenTokenRef}/>
-            {!state.success ?
+
+            <input
+                type="hidden"
+                name="g-recaptcha-response"
+                ref={hiddenTokenRef}
+            />
+
+            {!state.success ? (
                 <button
-                    className='border-[1.5px] border-black hover:border-zinc-700 p-3 shadow-8 text-zinc-950 hover:text-zinc-700'
+                    type="button"
+                    onClick={handleSubmit}
+                    className="border-[1.5px] border-black hover:border-zinc-700 p-3 shadow-8 text-zinc-950 hover:text-zinc-700"
                 >
                     Отправить
-                </button> :
+                </button>
+            ) : (
                 <div>Спасибо за ваш ответ!</div>
-            }
+            )}
         </form>
     );
 }
